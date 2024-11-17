75cbce3bb66f579eacfdabd99c691e6785f863b0
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-11-17 13:09:34 -0300

    updating example (using signals and slots)

--- studying/02/app/widgets/button_holder.py
+++ studying/02/app/widgets/button_holder.py
@@ -1,9 +1,15 @@
 from PySide6.QtWidgets import QMainWindow, QPushButton
 
+def button_clicked(action): # ação gerada quando recebemos um sinal
+    print("Clicou ai?")
+
 class ButtonHolder(QMainWindow): # herda de qmainwindow, podemos acessar métodos dessa classe
     def __init__(self): # construtor
         super().__init__()
         self.setWindowTitle("Button Holder App")
         button = QPushButton("Click me!")
+        button.setCheckable(True)
+
+        button.clicked.connect(button_clicked) # envio do sinal
 
         self.setCentralWidget(button)
